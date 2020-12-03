e848b3e86b6dcf4b8ac79809162348ad1ae2e5d0
PREMINDA <premindabandara0777@gmail.com>
2020-12-04 01:07:39 +0530

    fetchin data from databse

--- mongodb.js
+++ mongodb.js
@@ -1,37 +1,22 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
-
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+const {MongoClient,objectID}=require('mongodb')
 const connectionUrl ='mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
+
+ 
 MongoClient.connect(connectionUrl,{useNewUrlParser: true ,useUnifiedTopology: true},(error,client)=>{
     if(error){
         console.log('unable to connect to database')
     }
     const db = client.db(databaseName)
-
-    // db.collection('users').insertOne({
-    //     name: 'Preminda',
-    //     age: 23
-    // },(error,result)=>{
-    //     if(error){
-    //         console.log('Unable to insert')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-    db.collection('users').insertMany([
-        {
-            name:'preminda',
-            age: 21
-        },{
-            name:'Bandara',
-            age: 25
-        }
-    ],(error,result)=>{
+    db.collection('users').findOne({name:'preminda'},(error,user)=>{
         if(error){
-            console.log('Unable to connect')
+            console.log('unabl to fetch')
         }
-        console.log(result.ops)
+
+        console.log(user)
     })
+    
 })
